38b8890bc9b1e4cd148b32a385f22b689183fdf7
Randy Caballero <caballerorandy6@gmail.com>
2022-09-15 16:58:52 -0500

    Initialized project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Form from "./components/Form";
+import BookList from "./components/BookList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Form />
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
